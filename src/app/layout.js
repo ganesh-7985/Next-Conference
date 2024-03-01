e8797324f83506475d119090732680057abc98b7
ganesh-7985 <shankarganesh3005@gmail.com>
2024-03-01 21:46:11 +0530

    added About page

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,7 +2,7 @@ import { Inter } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
-import Scroll from "@/components/Scroll";
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -17,7 +17,6 @@ export default function RootLayout({ children }) {
       <body className={inter.className}>
       <Navbar/>
       {children}
-      <Scroll/>
       <Footer/>
       </body>
     </html>
